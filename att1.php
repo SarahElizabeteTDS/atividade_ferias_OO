e0a90c9ae4e97be7fd875d4b624dfbe437995a7c
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-07-05 01:20:07 -0300

    Update att1.php

--- att1.php
+++ att1.php
@@ -9,25 +9,26 @@ $memoria = 0;
 $pc = array();
 $pcs = array();
 
-//funcoes
+//funcao que imprime os dados
 function pipe($dado1, $dado2, $dado3, $dado4)
 {
     print $dado1 . " | " . $dado2 . " | " . $dado3 . " | " . $dado4 . "\n";
 }
 
+//laco para solicitar as informacoes
 for ($i=0; $i < 3; $i++) 
 { 
-    $processador = readline("Informe o modelo de processador de seu computador: ");
-    $placaVideo = readline("Informe o modelo de placa de vídeo: ");
-    $placaMae = readline("Informe o modelo de placa mãe: ");
-    $memoria = readline("Informe o modelo da memória RAM: ");
+    $processador = readline("Informe o modelo de processador do computador " . $i+1 . ": ");
+    $placaVideo = readline("Informe o modelo de placa de vídeo do computador " . $i+1 . ": ");
+    $placaMae = readline("Informe o modelo de placa mãe do computador " . $i+1 . ": ");
+    $memoria = readline("Informe o modelo da memória RAM do computador " . $i+1 . ": ");
 
     $pc = array("Processador" => $processador,"PlacaVideo" => $placaVideo, "PlacaMae" => $placaMae, "Memoria" => $memoria);
     array_push($pcs, $pc);
 }
 
+//percorre a matriz
 foreach($pcs as $pc)
 {
-    system("clear");
     pipe($pc["Processador"], $pc["PlacaVideo"], $pc["PlacaMae"], $pc["Memoria"]);
 }
